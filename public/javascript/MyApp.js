29630cff511e4ab44c80bc7a642197cd4df87637
unknown <mohammed.abrar95@gmail.com>
2018-02-25 13:23:56 +0530

    graph

--- public/javascript/MyApp.js
+++ public/javascript/MyApp.js
@@ -8,6 +8,7 @@ app.config(function($routeProvider) {
 			})
 			.when("/themes", {
 				templateUrl : "/views/themes.html",
+				controller : "themes"
 			})
 			.when("/journeys", {
 				templateUrl : "/views/journeys.html",
@@ -20,6 +21,28 @@ app.config(function($routeProvider) {
 			});
 	});
 
-app.controller('mycontroller', function($scope,$http) {	
-
+app.controller('themes', function($scope,$http) {	
+	var dataOfThemesByTime = [];
+		$http.get('/themesbytime').then(function(response){	
+			for(var i = 0 ; i < response.data.aggregations["0"].results.length ; i++)
+			{
+				dataOfThemesByTime.push({x: response.data.aggregations["0"].results[i].key_as_string , y: response.data.aggregations["0"].results[i].aggregations["0"].aggregations["0"].aggregations["0"].results["0"].aggregations["0"].value});
+				console.log(response.data.aggregations["0"].results[i].key_as_string);
+				console.log(response.data.aggregations["0"].results[i].aggregations["0"].aggregations["0"].aggregations["0"].results["0"].aggregations["0"].value);
+			}	
+			drawLineGraph('themesbytime',dataOfThemesByTime,['breakdown'],"Time");
+	});
 });
+
+function drawLineGraph(elementname, data, labels, xLabels){
+	Morris.Line({
+		element: elementname,
+		data: data,
+		xkey: 'x',
+		ykeys: 'y',
+		lineColors: ['#0ec888'],
+		labels: labels,
+		lineWidth : 7,
+		xLabels: xLabels
+	  });
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
-
+var DiscoveryV1 = require('watson-developer-cloud/discovery/v1');
+require('dotenv').config();
+var discovery = new DiscoveryV1({
+  username: process.env.DISCOVERYUSERNAME,
+  password: process.env.DISCOVERYPASSWORD,
+  version_date: '2017-11-07'
+});
 /* GET home page. */
+
+
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/themesbytime', function(req, res, next) {
+  discovery.query({ environment_id: 'a14383db-b897-4442-8232-948f70cfc5f4', collection_id: 'ee26da60-88a6-495b-be7c-50e79991fa85', query : '',
+aggregation : 'timeslice(time,1month,anomaly:true).nested(enriched_text.entities).filter(enriched_text.entities:(type:(ORGANIZATION|PRODUCT|PRICE|ORDER|PAYMENT|DELIVERY|SERVICE|EMPLOYEE),count:(1))).term(enriched_text.entities.type).[average(enriched_text.entities.sentiment.score),term(enriched_text.entities.sentiment.label)]' },
+ function(error, data) {
+  res.send(JSON.stringify(data, null, 2));
+  })
+});
 module.exports = router;
