6e81a49f5fe7726d4521d7d6ae74246a961310ba
SAMEER KHAN S <febsameer@gmail.com>
2018-03-13 01:16:21 +0530

    Motion Chart Added

--- public/javascript/MyApp.js
+++ public/javascript/MyApp.js
@@ -74,7 +74,7 @@ app.controller('themes', function ($scope, $http, $filter) {
 			}
 			$scope.datapoints[$scope.datapoints.length] = { 'group': response.data.aggregations["0"].results[i].key, datapoints: datapoints };
 		}
-		console.log($scope.datapoints);
+		// console.log($scope.datapoints);
 	})
 
 	$scope.drawtThemesByGroup = function (id, data, sentimentValue) {
@@ -145,6 +145,293 @@ app.controller('themes', function ($scope, $http, $filter) {
 			xLabels: xLabels
 		});
 	}
+
+function x(d) { return d.income; }
+function y(d) { return d.lifeExpectancy; }
+function radius(d) { return d.population; }
+function color(d) { return d.name; }
+function key(d) { return d.name; }
+function timeConverter(UNIX_timestamp){
+  var a = new Date(UNIX_timestamp);
+  //var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+  var year = a.getFullYear();
+//  var month = months[a.getMonth()];
+var month = a.getMonth();
+  var time = month + year ;
+//   return month;
+return UNIX_timestamp; //need to change this, json contains month, so returning as is.
+}
+// Chart dimensions.
+var margin = {top: 19.5, right: 19.5, bottom: 19.5, left: 39.5},
+    width = 800 - margin.right,
+    height = 300 - margin.top - margin.bottom;
+
+// Various scales. These domains make assumptions of data, naturally.
+var xScale = d3.scale.log().domain([3, 6]).range([0, width]),
+    yScale = d3.scale.linear().domain([-1, 1]).range([height, 0]),
+    radiusScale = d3.scale.sqrt().domain([0, 500]).range([1, 50]),
+    colorScale = d3.scale.category20();
+
+// The x & y axes.
+var xAxis = d3.svg.axis().orient("bottom").scale(xScale).ticks(12, d3.format(",d")),
+    yAxis = d3.svg.axis().scale(yScale).orient("left");
+
+// Create the SVG container and set the origin.
+var svg = d3.select("#themesRatingWithTime").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+    .attr("class", "gRoot")
+
+// Add the x-axis.
+svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
+
+// Add the y-axis.
+svg.append("g")
+    .attr("class", "y axis")
+    .call(yAxis);
+
+// Add an x-axis label.
+svg.append("text")
+    .attr("class", "x label")
+    .attr("text-anchor", "end")
+    .attr("x", width)
+    .attr("y", height - 6)
+    .text("Rating");
+
+// Add a y-axis label.
+svg.append("text")
+    .attr("class", "y label")
+    .attr("text-anchor", "end")
+    .attr("y", 6)
+    .attr("dy", ".75em")
+    .attr("transform", "rotate(-90)")
+    .text("Sentiment");
+
+// Add the year label; the value is set on transition.
+var label = svg.append("text")
+    .attr("class", "year label")
+    .attr("text-anchor", "end")
+    .attr("y", height - 24)
+    .attr("x", width)
+    .text(" ");
+
+// Add the country label; the value is set on transition.
+var countrylabel = svg.append("text")
+    .attr("class", "country label")
+    .attr("text-anchor", "start")
+    .attr("y", 80)
+    .attr("x", 20)
+    .text(" ");
+
+var first_time = true;
+
+// Load the data.
+d3.json("themesbyNpsApi2", function(nations) {
+
+  // A bisector since many nation's data is sparsely-defined.
+  var bisect = d3.bisector(function(d) { return d[0]; });
+    dragit.time = {min:d3.min(nations, function(d) { return d3.min(d.income, function(e) {return timeConverter(e[0]);})}), 
+	max:d3.max(nations, function(d) { return d3.max(d.income, function(e) {return timeConverter(e[0]);})}), 
+	step:1, current:d3.min(nations, function(d) { return d3.min(d.income, function(e) {return timeConverter(e[0]);})})}
+
+	
+  // Add a dot per nation. Initialize the data at 2006, and set the colors.
+  var dot = svg.append("g")
+      .attr("class", "dots")
+    .selectAll(".dot")
+      .data(interpolateData(dragit.time.max))
+    .enter().append("circle")
+      .attr("class", "dot")
+      .style("fill", function(d) { return colorScale(color(d)); })
+      .call(position)
+      .on("mousedown", function(d, i) {
+
+      })
+      .on("mouseup", function(d, i) {
+        dot.classed("selected", false);
+        d3.select(this).classed("selected", !d3.select(this).classed("selected"));
+        dragit.trajectory.display(d, i, "selected");
+
+        //TODO: test if has been dragged
+        // Look at the state machine history and find a drag event in it?
+
+      })
+      .on("mouseenter", function(d, i) {
+        clear_demo();
+        if(dragit.statemachine.current_state == "idle") {
+          dragit.trajectory.display(d, i)
+          dragit.utils.animateTrajectory(dragit.trajectory.display(d, i), dragit.time.current, 1000)
+          countrylabel.text(d.name);
+          dot.style("opacity", .4)
+          d3.select(this).style("opacity", 1)
+          d3.selectAll(".selected").style("opacity", 1)
+        }
+      })
+      .on("mouseleave", function(d, i) {
+
+        if(dragit.statemachine.current_state == "idle") {
+          countrylabel.text("");
+          dot.style("opacity", 1);
+        }
+  
+        dragit.trajectory.remove(d, i);
+      })
+      .call(dragit.object.activate)
+
+  // Add a title.
+  dot.append("title")
+      .text(function(d) { return d.name; });
+
+  // Start a transition that interpolates the data based on year.
+  svg.transition()
+      .duration(30000)
+      .ease("linear")
+
+  // Positions the dots based on data.
+  function position(dot) {
+    dot.attr("cx", function(d) { return xScale(x(d)); })
+       .attr("cy", function(d) { return yScale(y(d)); })
+       .attr("r", function(d) {  return radiusScale(radius(d)); });
+  }
+
+  // Defines a sort order so that the smallest dots are drawn on top.
+  function order(a, b) {
+    return radius(b) - radius(a);
+  }
+
+  // Updates the display to show the specified year.
+  function displayYear(year) {
+	  var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    dot.data(interpolateData(year+dragit.time.min), key).call(position);//.sort(order);
+    label.text(months[dragit.time.min + Math.round(year)]);
+  }
+
+  // Interpolates the dataset for the given (fractional) year.
+  function interpolateData(year) {
+    return nations.map(function(d) {
+      return {
+        name: d.name,
+        region: d.region,
+        income: interpolateValues(d.income, year),
+        population: interpolateValues(d.population, year),
+        lifeExpectancy: interpolateValues(d.lifeExpectancy, year)
+      };
+    });
+  }
+
+  // Finds (and possibly interpolates) the value for the specified year.
+  function interpolateValues(values, year) {
+  
+  if(values === undefined)
+	return 0;
+    var i = bisect.left(values, year, 0, values.length - 1),
+        a = values[i];
+    if (i > 0) {
+      var b = values[i - 1],
+          t = (year - a[0]) / (b[0] - a[0]);
+      return a[1] * (1 - t) + b[1] * t;
+    }
+    return a[1];
+  }
+  
+  init();
+
+  function update(v, duration) {
+  
+    dragit.time.current = v || dragit.time.current;
+    displayYear(dragit.time.current)
+    d3.select("#slider-time").property("value", dragit.time.current);
+  }
+
+  function init() {
+
+    dragit.init(".gRoot");
+	
+    dragit.data = d3.range(nations.length).map(function() { return Array(); })
+// console.log(dragit.time);
+    for(var yy = dragit.time.min; yy<dragit.time.max; yy++) {
+// console.log(yy);
+      interpolateData(yy).filter(function(d, i) { 
+	  
+        dragit.data[i][yy-dragit.time.min] = [xScale(x(d)), yScale(y(d))];
+
+      })
+    }
+
+    dragit.evt.register("update", update);
+
+    //d3.select("#slider-time").property("value", dragit.time.current);
+
+    d3.select("#slider-time")
+      .on("mousemove", function() { 
+        update(parseInt(this.value), 500);
+        clear_demo();
+      })
+
+    var end_effect = function() {
+      countrylabel.text("");
+      dot.style("opacity", 1)
+    }
+
+    dragit.evt.register("dragend", end_effect)
+  }
+
+function clear_demo() {
+  if(first_time) {
+     svg.transition().duration(0);
+    first_time = false;
+    window.clearInterval(demo_interval);
+    countrylabel.text("");
+    dragit.trajectory.removeAll();
+    d3.selectAll(".dot").style("opacity", 1)
+  }
+}
+
+function play_demo() {
+
+  var ex_nations = ["DELIVERY", "ORGANIZATION", "SERVICE", "ORDER", "PRODUCT", "REFUND"]
+  var index_random_nation = null;
+  var random_index = Math.floor(Math.random() * ex_nations.length);
+  var random_nation = nations.filter(function(d, i) { 
+    if(d.name == ex_nations[random_index]) {
+      index_random_nation = i;
+      return true;
+    }
+  })[0];
+
+//   console.log(random_nation);
+  var random_nation = nations[index_random_nation];
+
+  dragit.trajectory.removeAll();
+  dragit.trajectory.display(random_nation, index_random_nation);
+  countrylabel.text(random_nation.name);
+
+  dragit.utils.animateTrajectory(dragit.lineTrajectory, dragit.time.min, 2000)
+
+  d3.selectAll(".dot").style("opacity", .4)
+
+  d3.selectAll(".dot").filter(function(d) {
+    return d.name == random_nation.name;
+  }).style("opacity", 1)
+}
+
+var demo_interval = null;
+
+setTimeout(function() {
+  if(first_time) {
+	//   console.log("1");
+    play_demo()
+    demo_interval = setInterval(play_demo, 3000)
+  }
+}, 1000);
+
+});
+
 });
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var _ = require('underscore');
 var router = express.Router();
 var DiscoveryV1 = require('watson-developer-cloud/discovery/v1');
 require('dotenv').config();
@@ -9,7 +10,52 @@ var discovery = new DiscoveryV1({
 });
 /* GET home page. */
 
+function timeConverter(UNIX_timestamp){
+  var a = new Date(UNIX_timestamp);
+  //var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+  var year = a.getFullYear();
+//  var month = months[a.getMonth()];
+var month = a.getMonth();
+  var time = month + year ;
+  return month;
+}
 
+function rdata(res1, res2, res3) {
+  var sda1 = [];
+  var sda2 = [];
+  var sda3 = [];
+  var final = [];
+  var res = res1;
+  for (var i = 0; i < res.length; i++) {
+    for (var j = 0; j < res[i].val[0].length; j++) {
+        if (!sda1[res[i].val[0][j].name])
+        sda1[res[i].val[0][j].name] = [];
+        sda1[res[i].val[0][j].name][sda1[res[i].val[0][j].name].length] = [res[i].key, res[i].val[0][j].value];
+    }
+}
+  
+  res = res2;
+  for (var i = 0; i < res.length; i++) {
+      for (var j = 0; j < res[i].val.length; j++) {
+          if (!sda2[res[i].val[j].name])
+              sda2[res[i].val[j].name] = [];
+          sda2[res[i].val[j].name][sda2[res[i].val[j].name].length] = [res[i].key, res[i].val[j].value];
+      }
+  }
+
+  for (var i = 0; i < res3.length; i++) {
+    for (var j = 0; j < res3[i].val[0].length; j++) {
+        if (!sda3[res3[i].val[0][j].name])
+        sda3[res3[i].val[0][j].name] = [];
+        sda3[res3[i].val[0][j].name][sda3[res3[i].val[0][j].name].length] = [res3[i].key, res3[i].val[0][j].value];
+    }
+}
+  for (var i = 0; i < res[0].val.length; i++) {
+      final.push({ name: res[0].val[i].name, lifeExpectancy: sda2[res[0].val[i].name], population: sda1[res[0].val[i].name], income: sda3[res[0].val[i].name] })
+  }
+  return(final);
+
+}
 
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
@@ -44,13 +90,38 @@ router.get('/ ', function (req, res, next) {
 });
 
 router.get('/themesbyNpsApi2', function (req, res, next) {
-  discovery.query({
-    environment_id: 'a14383db-b897-4442-8232-948f70cfc5f4', collection_id: 'ee26da60-88a6-495b-be7c-50e79991fa85', query: '',
-    aggregation: 'timeslice(time,1month,anomaly:true).term(enriched_text.entities.type).average(nps_rating)'
-  },
-    function (error, data) {
-      res.send(JSON.stringify(data, null, 2));
-    })
+    discovery.query({
+      environment_id: 'a14383db-b897-4442-8232-948f70cfc5f4', collection_id: 'ee26da60-88a6-495b-be7c-50e79991fa85', query: '',
+      aggregation: 'timeslice(time,1month,anomaly:true).term(enriched_text.entities.type,count:50).average(nps_rating)'
+    },
+      function (error, d1) {
+        
+        if(!error)
+        {
+          discovery.query({
+            environment_id: 'a14383db-b897-4442-8232-948f70cfc5f4', collection_id: 'ee26da60-88a6-495b-be7c-50e79991fa85', query: '',
+            aggregation: 'timeslice(time,1month,anomaly:true).nested(enriched_text.entities).filter(enriched_text.entities:(type:(PROMOTION|ORGANIZATION|PRODUCT|PRICE|ORDER|PAYMENT|DELIVERY|SERVICE|EMPLOYEE|REFUND|RETURN|BONUS),count:(1))).term(enriched_text.entities.type).average(enriched_text.entities.sentiment.score)'
+          },
+            function (error2, d2) {
+              if(!error2)
+              {
+                  var sda1 = d1.aggregations[0].results;      
+                  var sda2 = d2.aggregations[0].results;
+                  /* returns timestamp
+                  var dat1 = _.map(sda1, function(a){ return {key: a.key , val: _.map(a.aggregations, function(c) {return _.map(c.results, function(d) {return { name: d.key, value: d.matching_results}})})}});
+                  var dat2 = _.map(sda2, function(a){ return {key: a.key , val: _.map(a.aggregations, function(c) {return _.map(c.aggregations, function(e) {return _.map(e.aggregations, function(b) {return _.map(b.results, function(d) {return { name: d.key, value: _.map(d.aggregations, function(f) {return f.value})[0]}})})[0]})[0]})[0]}});
+                  var dat3 = _.map(sda1, function(a){ return {key: a.key , val: _.map(a.aggregations, function(c) {return _.map(c.results, function(d) {return { name: d.key, value: _.map(d.aggregations, function(z) { return z.value})}})})}});
+                  */
+                  //Returns month
+                  var dat1 = _.map(sda1, function(a){ return {key: timeConverter(a.key) , val: _.map(a.aggregations, function(c) {return _.map(c.results, function(d) {return { name: d.key, value: d.matching_results}})})}});
+                  var dat2 = _.map(sda2, function(a){ return {key: timeConverter(a.key) , val: _.map(a.aggregations, function(c) {return _.map(c.aggregations, function(e) {return _.map(e.aggregations, function(b) {return _.map(b.results, function(d) {return { name: d.key, value: _.map(d.aggregations, function(f) {return f.value})[0]}})})[0]})[0]})[0]}});
+                  var dat3 = _.map(sda1, function(a){ return {key: timeConverter(a.key) , val: _.map(a.aggregations, function(c) {return _.map(c.results, function(d) {return { name: d.key, value: _.map(d.aggregations, function(z) { return z.value})}})})}});
+
+                  res.send(JSON.stringify(rdata(dat1,dat2,dat3), null, 2));
+              }
+            });
+        }
+      });    
 });
 
 
